3a34eef94849af73a1997188ced630cd0667ad76
E. Nilges <emilia.nilges@stud.h-da.de>
2015-07-17 17:33:16 +0200

    moved work to this file

---
+++ Assets/Scripts/FollowCam.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowCam : MonoBehaviour {
+
+
+	public static FollowCam S; // Singleton Follow Cam Instance
+
+	public GameObject poi;
+
+	private float camZ;
+	public float easing = 0.05f;
+
+	public Vector2 minXY;
+
+
+	void Awake() {
+		S = this;
+		camZ = transform.position.z;
+	}
+
+	void FixedUpdate() {
+
+		Vector3 destination;
+
+		// Check if the poi exists
+		if(poi == null) {
+			// set the destination to the slingshot (zero  vector)
+			destination = Vector3.zero;
+		} else {
+			// else (there is a poi)
+
+			destination = poi.transform.position;
+
+			// is the poi a projectile ?
+			if(poi.tag == "Projectile") {
+
+				// check if it is resting (sleeping)
+				if(poi.GetComponent<Rigidbody>().IsSleeping()  ){
+			
+					// set the poi to default (null)
+					poi = null;
+					return;
+				}
+
+			}
+		}
+
+		destination.x = Mathf.Max (minXY.x, destination.x);
+		destination.y = Mathf.Max (minXY.y, destination.y);
+
+		destination = Vector3.Lerp (transform.position, destination, easing);
+
+		destination.z = camZ;
+
+		transform.position = destination;
+
+		this.GetComponent<Camera>().orthographicSize = 10 + destination.y;
+	
+	}
+
+
+
+}
+
+
+
+
+
+
---
+++ Assets/Scripts/Goal.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Goal : MonoBehaviour {
+
+	// Static field accessible from anywhere
+	public static bool goalMet;
+
+	void OnTriggerEnter(Collider other) {
+		// Check if the object is a projectile
+		if(other.gameObject.tag == "Projectile") {
+			// If so, set goalMet to true
+			goalMet = true;
+
+			// Also set the goals alpha to a higher opacity
+			// (use Renderer component)
+			Color c = this.GetComponent<Renderer>().material.color;
+
+			c.a = 1;
+
+			this.GetComponent<Renderer>().material.color = c;
+
+
+		}
+	}
+
+
+
+}
+
+
+
+
+
+
+
